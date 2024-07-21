42fcba4ad893cf127ebdfc4d2a11d1344234c9b6
CTHartze <ismailmaho007@gmail.com>
2024-07-21 23:25:18 +0200

    port and server connection setup

--- server/index.js
+++ server/index.js
@@ -3,3 +3,15 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
 const app = express();
+//converts data into json format
+app.use(express.json());
+//Port
+const PORT = process.env.PORT || 5500;
+
+
+
+
+
+
+//Adds port & connects to server
+app.listen(PORT, ()=> console.log("Server connected") );
