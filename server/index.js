773d3e75cfe8963cafce9e6cdae68fb506c9454e
CTHartze <ismailmaho007@gmail.com>
2024-07-22 21:11:42 +0200

    continuing first route creation

--- server/index.js
+++ server/index.js
@@ -8,8 +8,17 @@ app.use(express.json());
 //Port
 const PORT = process.env.PORT || 5500;
 
+//imports routes
+const TodoItemRoute = require('./routes/todoItems');
 
 
+//connecting to mongodb
+mongoose.connect(process.env.DB_CONNECT)
+.then(()=> console.log("Database connected"))
+.catch(err => console.log(err))
+
+
+app.use('/', TodoItemRoute);
 
 
 
--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -3,4 +3,22 @@ const router = require('express').Router();
 const todoItemsModel = require('../models/todoItems');
 
 
-//
+//creating first route -- adding Todo Item
+router.post('/api/item', async (req, res)=>{
+    try{
+        const newItem = new todoItemsModel({
+          item: req.body.item
+        })
+        //saves item in database
+        const saveItem = await newItem.save()
+        res.status(200).json('Item Added Successfully.');
+    }catch(err){
+        res.json(err);
+    }
+})
+
+
+
+
+//exports router
+module.exports = router;
