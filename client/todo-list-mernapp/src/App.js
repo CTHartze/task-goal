3366505ec432e9fa48eef8cdfa10ae58295359b2
CTHartze <ismailmaho007@gmail.com>
2024-07-23 19:29:52 +0200

    installed axios to add functionality to add new items to the database, and cors for host access

--- client/todo-list-mernapp/src/App.js
+++ client/todo-list-mernapp/src/App.js
@@ -1,12 +1,28 @@
-
+import {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [itemText, setItemText] = useState('')
+
+  //adds new item to database
+  const addItem = async (e) => {
+    e.preventDefault();
+    try{
+      const res = await axios.post('http://localhost:5500/api/item', {item: itemText})
+      console.log(res);
+      setItemText('');
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+
   return (
     <div className="App">
       <h1>Task Checklist</h1>
-      <form className="form">
-        <input type="text" placeholder='Add Checklist Item' />
+      <form className="form" onSubmit={e => addItem(e)}>
+        <input type="text" placeholder='Add Checklist Item' onChange={e => {setItemText(e.target.value)} } value={itemText} />
         <button type="submit">Add</button>
       </form>
       <div className="todo-listItems">
--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 //converts data into json format
 app.use(express.json());
 //Port
 const PORT = process.env.PORT || 5500;
+
+//use cors
+app.use(cors());
 
 //imports routes
 const TodoItemRoute = require('./routes/todoItems');
